998b55106ed424a951bb8d5a1825c049edca6a5f
cabrazil <cabrazil@gmail.com>
2025-07-08 17:09:10 -0300

    ux responsivo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -150,10 +150,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       
-      <div className="min-h-screen bg-gray-50">
+      <div className="min-h-screen bg-gray-50 flex flex-col">
         <Header />
 
-        <main>
+        <main className="flex-grow">
           <HeroImage />
 
           <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8 py-16">
